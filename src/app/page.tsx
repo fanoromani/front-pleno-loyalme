ea7ce894f52d3740be7562d8d2bcbd6a95641dd2
fanoromani <fanoromani@gmail.com>
2025-09-01 23:36:59 -0300

    Update: Implement CouponList to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import CouponList from "@/components/CouponList";
+import { fetchCoupons } from "@/service/fetchCoupons";
+
+export default async function Home() {
+  const coupons = await fetchCoupons();
+
+  if (!coupons?.length) {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-center bg-amber-400">
+        <h1 className="text-3xl font-bold underline my-8">
+          Nenhum cupom disponível
+        </h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-amber-400">
+      <h1 className="text-3xl font-bold underline my-8">Cupons</h1>
+      <div className="bg-gray-100 p-5">
+        <CouponList coupons={coupons} />
+      </div>
     </div>
   );
 }
